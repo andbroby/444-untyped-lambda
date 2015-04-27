72ca1197b1a7e76d966fdab5572eec7567543c12
andbroby <and.broby@gmail.com>
2015-04-27 16:45:08 +0200

    some eval stuff

--- src/Main.hs
+++ src/Main.hs
@@ -18,11 +18,23 @@ shift d c t = case t of
 subst :: Integer -> Term -> Term -> Term
 subst j s t = case t of
   arg@(Arg n) -> if n==j then s else arg
-  Abs t -> subst (j+1) (shift 1 0 s) t
+  Abs t -> Abs(subst (j+1) (shift 1 0 s) t)
   App t1 t2 -> App (subst j s t1) (subst j s t2)
-  
+
+substTop s t = shift (-1) 0 (subst 0 (shift 1 0 s) t)
+
+isval :: Term -> Bool
+isval (Abs _) = True
+isval _ = False
+
+eval :: Term -> Term
+eval (App t v) | isval v = substTop v t
+               | isval t = substTop t v
+               | otherwise = undefined
+                      
 main = do
-  let x = (Arg 1)
-      y = subst 1 (Arg 3) x
-  putStrLn . show $ y
-  putStrLn . show $ x
+  let x = Arg 0
+      y = Abs(Arg(1))
+      z = App x y
+  putStrLn . show $ z
+  putStrLn . show $ subst 0 (App (Arg 1) (Abs (Arg 2 ))) z
