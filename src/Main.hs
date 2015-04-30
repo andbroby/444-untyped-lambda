94d9e31ea2e764cac65df5602c5e0ea38d8d1157
andbroby <and.broby@gmail.com>
2015-04-30 12:50:24 +0200

    fix some eval logic

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ data Term = Arg Integer
 instance Show Term where
   show (Arg n) = show n
   show (Abs t) = "λ. " ++ show t
-  show (App t1 t2) = show t1 ++ " " ++ show t2
+  show (App t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
 
 shift :: Integer -> Integer -> Term -> Term
 shift d c t = case t of
@@ -18,23 +18,21 @@ shift d c t = case t of
 subst :: Integer -> Term -> Term -> Term
 subst j s t = case t of
   arg@(Arg n) -> if n==j then s else arg
-  Abs t -> Abs(subst (j+1) (shift 1 0 s) t)
+  Abs t -> subst (j+1) (shift 1 0 s) t
   App t1 t2 -> App (subst j s t1) (subst j s t2)
 
-substTop s t = shift (-1) 0 (subst 0 (shift 1 0 s) t)
+eval :: Term -> Term -> Term
+eval t v = shift (-1) 0 (subst 0 (shift 1 0 v) t)
 
 isval :: Term -> Bool
 isval (Abs _) = True
 isval _ = False
-
-eval :: Term -> Term
-eval (App t v) | isval v = substTop v t
-               | isval t = substTop t v
-               | otherwise = undefined
                       
 main = do
-  let x = Arg 0
-      y = Abs(Arg(1))
-      z = App x y
-  putStrLn . show $ z
-  putStrLn . show $ subst 0 (App (Arg 1) (Abs (Arg 2 ))) z
+  let x = Arg 1
+      y = Arg 0
+      z = Arg 2
+      q = Abs (App x (App y z))
+      l = Abs q
+  putStrLn . show $ l
+  putStrLn . show $ shift 2 0 l
